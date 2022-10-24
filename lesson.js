400b51a0a499e3b7d7ecc1c4a3f3f807d215afcb
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-10-24 21:36:27 +0300

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -2810,4 +2810,445 @@ const getUserWithEmail = (users, email) => {return users.find(user => user.email
 };
 // Change code above this line
 
+  //----------------------------------------------------------------------------------------------------------
+  //Метод every(callback) проверяет проходят ли все элементы массива тест предоставляемый коллбек-функцией. //Возвращает true или false.
+
+//Используя метод every() дополни код так, чтобы:
+
+//    В переменной eachElementInFirstIsEven был результат проверки всех элементов массива firstArray на чётность.
+//    В переменной eachElementInFirstIsOdd был результат проверки всех элементов массива firstArray на нечётность.
+//    В переменной eachElementInSecondIsEven был результат проверки всех элементов массива secondArray на чётность.
+//    В переменной eachElementInSecondIsOdd был результат проверки всех элементов массива secondArray на нечётность.
+ //   В переменной eachElementInThirdIsEven был результат проверки всех элементов массива thirdArray на чётность.
+//    В переменной eachElementInThirdIsOdd был результат проверки всех элементов массива thirdArray на нечётность.
+
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change code below this line
+
+const eachElementInFirstIsEven = firstArray.every(firstArrayElement => firstArrayElement % 2 === 0);
+const eachElementInFirstIsOdd = firstArray.every(firstArrayElement => firstArrayElement % 2 === 1);
+
+const eachElementInSecondIsEven =  secondArray.every(secondArrayElement => secondArrayElement % 2 === 0);
+const eachElementInSecondIsOdd = secondArray.every(secondArrayElement => secondArrayElement % 2 === 1);
+
+const eachElementInThirdIsEven = thirdArray.every(thirdArrayElement => thirdArrayElement % 2 === 0);
+const eachElementInThirdIsOdd = thirdArray.every(thirdArrayElement => thirdArrayElement % 2 === 1);
+console.log(eachElementInFirstIsEven);
+console.log(eachElementInFirstIsOdd);
+
+console.log(eachElementInSecondIsEven);
+console.log(eachElementInSecondIsOdd);
+
+console.log(eachElementInThirdIsEven);
+console.log(eachElementInThirdIsOdd);
+//
+   // Объявлена переменная firstArray
+   // Значение переменной firstArray это массив [26, 94, 36, 18]
+   // Объявлена переменная secondArray
+   // Значение переменной secondArray это массив [17, 61, 23]
+   // Объявлена переменная thirdArray
+   // Значение переменной thirdArray это массив [17, 26, 94, 61, 36, 23, 18]
+   // Объявлена переменная eachElementInFirstIsEven
+   // Значение переменной eachElementInFirstIsEven это буль true
+   // Объявлена переменная eachElementInFirstIsOdd
+  //  Значение переменной eachElementInFirstIsOdd это буль false
+  //  Объявлена переменная eachElementInSecondIsEven
+  //  Значение переменной eachElementInSecondIsEven это буль false
+ //  Объявлена переменная eachElementInSecondIsOdd
+ //  Значение переменной eachElementInSecondIsOdd это буль true
+ //   Объявлена переменная eachElementInThirdIsEven
+  //  Значение переменной eachElementInThirdIsEven это буль false
+  //  Объявлена переменная eachElementInThirdIsOdd
+  //  Значение переменной eachElementInThirdIsOdd это буль false
+  //  Для перебора массивов использован метод every()
+
+  //--------------------------------------------------------------------------------------------------------
+
+//Дополни функцию isEveryUserActive(users) так, чтобы она проверяла все ли пользователи сейчас активны //(свойство isActive) и возвращала true или false.
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+// Change code below this line
+const isEveryUserActive = (users) => {return users.every(user => user.isActive);};
+   
+
+console.log(isEveryUserActive(users));
+//
+ //   Объявлена переменная isEveryUserActive
+ //   Переменной isEveryUserActive присвоена стрелочная функция с параметром (users)
+ //   Для перебора параметра users используется метод every()
+  //  Вызов функции с указанным массивом пользователей возвращает false
+  //  Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+  //------------------------------------------------------------------------------------------------------
+  //Используя метод some() дополни код так, чтобы:
+
+//    В переменной anyElementInFirstIsEven был результат проверки наличия чётных элементов в массиве firstArray.
+//    В переменной anyElementInFirstIsOdd был результат проверки наличия нечётных элементов в массиве firstArray.
+//    В переменной anyElementInSecondIsEven был результат проверки наличия чётных элементов в массиве secondArray.
+//    В переменной anyElementInSecondIsOdd был результат проверки наличия нечётных элементов в массиве secondArray.
+//    В переменной anyElementInThirdIsEven был результат проверки наличия чётных элементов в массиве thirdArray.
+//    В переменной anyElementInThirdIsOdd был результат проверки наличия нечётных элементов в массиве thirdArray.
+
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change code below this line
+
+const anyElementInFirstIsEven = firstArray.some(firstArrayElement => firstArrayElement % 2 === 0);
+const anyElementInFirstIsOdd = firstArray.some(firstArrayElement => firstArrayElement % 2 === 1);
+
+const anyElementInSecondIsEven =  secondArray.some(secondArrayElement => secondArrayElement % 2 === 0);
+const anyElementInSecondIsOdd = secondArray.some(secondArrayElement => secondArrayElement % 2 === 1);
+
+const anyElementInThirdIsEven = thirdArray.some(thirdArrayElement => thirdArrayElement % 2 === 0);
+const anyElementInThirdIsOdd = thirdArray.some(thirdArrayElement => thirdArrayElement % 2 === 1);
+console.log(anyElementInFirstIsEven);
+console.log(anyElementInFirstIsOdd);
+
+console.log(anyElementInSecondIsEven);
+console.log(anyElementInSecondIsOdd);
+
+console.log(anyElementInThirdIsEven);
+  console.log(anyElementInThirdIsOdd);
+  //-------------------------------------------------------------------------------------------------------
+////Дополни функцию isAnyUserActive(users) так, чтобы она проверяла наличие активных пользователей (свойство //isActive) и возвращала true или false.
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+// Change code below this line
+const isAnyUserActive = (users) => {return users.some(user => user.isActive);};
+   
+
+  console.log(isAnyUserActive(users));
+// Change code below this line
+//----------------------------------------------------------------------------------------------------------
+  
+  //Метод reduce(callback, initialValue) используется для последовательной обработки каждого элемента массива с //сохранением промежуточного результата, как аккумулятор. Немного сложнее других в усвоении, но результат стоит //того.
+//Игровому сервису необходим функционал подсчёта среднего времени проведённого в играх. Дополни код так, чтобы //в переменной totalPlayTime получилось общее игровое время из массива playtimes.
+const players = {
+  mango: 1270,
+  poly: 468,
+  ajax: 710,
+  kiwi: 244
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// Change code below this line
+
+const totalPlayTime = playtimes.reduce((a, b) => (a + b),0);
+console.log(totalPlayTime);
+
+// Change code above this line
+const averagePlayTime =  totalPlayTime / playtimes.length;
+
+   // Объявлена переменная players
+   // Значение переменной players это объект игроков с игровым временем каждого
+   // Объявлена переменная playtimes
+   // Значение переменной playtimes это массив [1270, 468, 710, 244]
+   // Объявлена переменная totalPlayTime
+   // Значение переменной totalPlayTime это число 2692
+   // Для перебора массива playtimes используется метод reduce()
+   // Объявлена переменная averagePlayTime
+   // Значение переменной averagePlayTime это число 673
+  //------------------------------------------------------------------------------------------------------
+  //Нашему сервису необходимо рассчитать среднее время проведённое в одной игре для каждого игрока, и получить //общую сумму этих времён. Рассчитать время для каждого из игроков, можно разделив его время (свойство //playtime) на количество игр (свойство gamesPlayed).
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
+
+const totalAveragePlaytimePerGame = players.reduce((a, player) => a + player.playtime / player.gamesPlayed, 0);
+console.log(totalAveragePlaytimePerGame);
+//
+//    Объявлена переменная players
+//    Значение переменной players это массив объектов игроков
+ //   Объявлена переменная totalAveragePlaytimePerGame
+//    Значение переменной totalAveragePlaytimePerGame это число 1023
+//    Для перебора массива players используется метод reduce()
+  
+  //--------------------------------------------------------------------------------------------------------
+  //Дополни функцию calculateTotalBalance(users) так, чтобы она считала и возвращала сумму всех средств //(свойство balance) которые хранят пользователи из массива users.
+const users =[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+// Change code below this line
+const calculateTotalBalance = users => {return users.reduce((a, user) => a + user.balance, 0)
+  
+};
+console.log(calculateTotalBalance(users)); 
+// Change code above this line
+//
+//    Объявлена переменная calculateTotalBalance
+//    Переменной calculateTotalBalance присвоена стрелочная функция с параметром (users)
+//    Для перебора параметра users используется метод reduce()
+ //   Вызов функции с указанным массивом пользователей возвращает число 20916
+ //   Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+  //--------------------------------------------------------------------------------------------
+  //Дополни функцию getTotalFriendCount(users) так, чтобы она считала и возвращала общее количество друзей //(свойство friends) всех пользователей из массива users.
+const users =[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+const getTotalFriendCount = users => {return users.reduce((a, user) => a + user.friends.length, 0)
+};
+console.log(getTotalFriendCount(users)); 
+
+// 
+ //   Объявлена переменная getTotalFriendCount
+  //  Переменной getTotalFriendCount присвоена стрелочная функция с параметром (users)
+  //  Для перебора параметра users используется метод reduce()
+    //Вызов функции с указанным массивом пользователей возвращает число 14
+    //Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
 }
